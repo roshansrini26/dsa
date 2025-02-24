5522cdfd093c9177b40850f2e22c60972492f92e
roshansrini26 <roshansrini26@gmail.com>
2025-02-24 12:06:44 +0530

    array_3

--- array_1.py
+++ array_1.py
@@ -228,4 +228,48 @@ class Solution:
             word_to_char[word] = char
         return True
 
-    
+
+#rotate array
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+
+        def rev(start,end):
+            while start < end:
+                nums[start] , nums[end] = nums[end] , nums[start]
+                start += 1
+                end -= 1
+        
+        rev(0,n-1)
+
+        rev(0,k-1)
+
+        rev(k,n-1)
+
+nums = [4,5,6,8,9,7,1,2]
+sol = Solution()
+sol.rotate(nums,3)
+print(nums)
+
+#intersection of array:
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count_map = {}
+        res = []
+
+        for num in nums1:
+            if num in count_map:
+                count_map[num] += 1
+            else:
+                count_map[num] = 1
+        
+        for num in nums2:
+            if num in count_map and count_map[num] >0:
+                res.append(num)
+                count_map[num] -= 1
+        return res
+
