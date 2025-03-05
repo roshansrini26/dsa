284b52747db6db6ea12f0faf650d60912755ae0f
roshansrini26 <roshansrini26@gmail.com>
2025-03-05 16:02:57 +0530

    string_1

--- strings.py
+++ strings.py
@@ -125,3 +125,26 @@ def isIsomorphic(s: str, t: str) -> bool:
     print("\nNo conflicts found, strings are isomorphic.")
     return True  # Step 4: No conflicts found
 isIsomorphic("foo", "bar")
+
+#Multiply strings sithout using any built-in functions
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        m , n = len(num1) , len(num2)
+        res = [0] * (m + n)
+
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                mul = (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+                pos1, pos2 = i+j, i+j+1
+                total = mul + res[pos2]
+
+                res[pos2] = total %10
+                res[pos1] += total // 10
+
+        res_str = "".join(map(str,res)).lstrip("0")
+        return res_str if res_str else "0"
+
+sol = Solution()
+print(sol.multiply("123","456"))
+
